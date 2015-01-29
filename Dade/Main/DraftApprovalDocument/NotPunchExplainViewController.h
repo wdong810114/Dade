55dd12918a0502f76e0decf8dc2bb1071bb929f0
王冬冬 <wdong810114@sina.com>
2015-01-29 16:55:29 +0800

    Modified not punch explain page.

--- Dade/Main/DraftApprovalDocument/NotPunchExplainViewController.h
+++ Dade/Main/DraftApprovalDocument/NotPunchExplainViewController.h
@@ -11,4 +11,23 @@
 
 @interface NotPunchExplainViewController : BaseViewController
 
+@property (weak, nonatomic) IBOutlet UIScrollView *notPunchExplainScrollView;
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *departmentLabel;
+@property (weak, nonatomic) IBOutlet UIView *numberView;
+@property (weak, nonatomic) IBOutlet UITextField *numberTextField;
+@property (weak, nonatomic) IBOutlet UIView *dateView;
+@property (weak, nonatomic) IBOutlet UITextField *dateTextField;
+@property (weak, nonatomic) IBOutlet UIView *notPunchView;
+@property (weak, nonatomic) IBOutlet UITextView *notPunchTextView;
+@property (weak, nonatomic) IBOutlet UILabel *notPunchPlaceholderLabel;
+@property (weak, nonatomic) IBOutlet UIView *explainView;
+@property (weak, nonatomic) IBOutlet UITextView *explainTextView;
+@property (weak, nonatomic) IBOutlet UILabel *explainPlaceholderLabel;
+@property (weak, nonatomic) IBOutlet UIButton *reportButton;
+@property (weak, nonatomic) IBOutlet UIView *approvalView;
+
+- (IBAction)reportButtonClicked:(UIButton *)button;
+
 @end
--- Dade/Main/DraftApprovalDocument/NotPunchExplainViewController.m
+++ Dade/Main/DraftApprovalDocument/NotPunchExplainViewController.m
@@ -10,13 +10,46 @@
 
 @interface NotPunchExplainViewController ()
 
+- (void)initView;
+- (BOOL)checkValidity;
+- (void)report;
+
 @end
 
 @implementation NotPunchExplainViewController
 
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+}
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if(self) {
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(keyboardWillShow:)
+                                                     name:UIKeyboardWillShowNotification
+                                                   object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(keyboardWillHide:)
+                                                     name:UIKeyboardWillHideNotification
+                                                   object:nil];
+    }
+    
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self initView];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,6 +68,163 @@
 - (void)backClicked:(UIButton *)button
 {
     [self pop];
+}
+
+- (IBAction)reportButtonClicked:(UIButton *)button
+{
+    // 呈报
+    
+    [self report];
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    CGRect keyboardFrame = [[notification.userInfo valueForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGFloat maxOffsetY, minOffsetY;
+    
+    if([self.numberTextField isFirstResponder]) {
+        maxOffsetY = self.numberView.frame.origin.y;
+        minOffsetY = self.numberView.frame.origin.y + self.numberView.frame.size.height + keyboardFrame.size.height - self.notPunchExplainScrollView.frame.size.height;
+    } else if([self.dateTextField isFirstResponder]) {
+        maxOffsetY = self.dateView.frame.origin.y;
+        minOffsetY = self.dateView.frame.origin.y + self.dateView.frame.size.height + keyboardFrame.size.height - self.notPunchExplainScrollView.frame.size.height;
+    } else if([self.notPunchTextView isFirstResponder]) {
+        maxOffsetY = self.notPunchView.frame.origin.y;
+        minOffsetY = self.notPunchView.frame.origin.y + self.notPunchView.frame.size.height + keyboardFrame.size.height - self.notPunchExplainScrollView.frame.size.height;
+    } else if([self.explainTextView isFirstResponder]) {
+        maxOffsetY = self.explainView.frame.origin.y;
+        minOffsetY = self.explainView.frame.origin.y + self.explainView.frame.size.height + keyboardFrame.size.height - self.notPunchExplainScrollView.frame.size.height;
+    }
+    
+    if(maxOffsetY < self.notPunchExplainScrollView.contentOffset.y) {
+        [self.notPunchExplainScrollView setContentOffset:CGPointMake(0.0, maxOffsetY) animated:YES];
+    }
+    if(self.notPunchExplainScrollView.contentOffset.y < minOffsetY) {
+        [self.notPunchExplainScrollView setContentOffset:CGPointMake(0.0, minOffsetY) animated:YES];
+    }
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification
+{
+    CGFloat scrollMaxOffsetY = self.notPunchExplainScrollView.contentSize.height - self.notPunchExplainScrollView.frame.size.height;
+    if(self.notPunchExplainScrollView.contentOffset.y > scrollMaxOffsetY) {
+        [self.notPunchExplainScrollView setContentOffset:CGPointMake(0.0, scrollMaxOffsetY) animated:YES];
+    }
+}
+
+- (void)dismissKeyboard
+{
+    if([self.notPunchTextView isFirstResponder]) {
+        [self.notPunchTextView resignFirstResponder];
+    } else if([self.explainTextView isFirstResponder]) {
+        [self.explainTextView resignFirstResponder];
+    }
+}
+
+#pragma mark - Private Methods
+- (void)initView
+{
+    if(!IOS_VERSION_7_OR_ABOVE) {
+        self.nameLabel.preferredMaxLayoutWidth = self.nameLabel.bounds.size.width;
+        self.departmentLabel.preferredMaxLayoutWidth = self.departmentLabel.bounds.size.width;
+    }
+    
+    [self.reportButton setBackgroundImage:[Util imageWithColor:RED_BUTTON_BG_NORMAL_COLOR] forState:UIControlStateNormal];
+    [self.reportButton setBackgroundImage:[Util imageWithColor:RED_BUTTON_BG_HIGHLIGHTED_COLOR] forState:UIControlStateHighlighted];
+    [self.reportButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    
+    UIToolbar *inputAccessoryView = [[UIToolbar alloc]initWithFrame:CGRectMake(0.0, 0.0, DEVICE_WIDTH, INPUT_ACCESSORY_VIEW_HEIGHT)];
+    inputAccessoryView.barStyle = UIBarStyleDefault;
+    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:BUTTON_TITLE_DONE style:UIBarButtonItemStyleDone target:self action:@selector(dismissKeyboard)];
+    NSArray *buttonArray = [NSArray arrayWithObjects:flexibleSpace, doneButton, nil];
+    inputAccessoryView.items = buttonArray;
+    self.notPunchTextView.inputAccessoryView = inputAccessoryView;
+    self.explainTextView.inputAccessoryView = inputAccessoryView;
+    
+    // 测试---Start
+    self.nameLabel.text = @"王冬王冬冬王冬冬王冬冬王冬冬王冬冬王冬冬王冬冬王冬冬";
+    self.departmentLabel.text = @"综合管理部综合管理部综合管理部综合管理部综合管理部综合管理部综合管理部综合管理部";
+    // 测试---End
+}
+
+- (BOOL)checkValidity
+{
+    if([[Util trimString:self.numberTextField.text] isEqualToString:@""]) {
+        [self showAlert:@"考勤号不能为空"];
+        
+        return NO;
+    }
+    
+    if([[Util trimString:self.dateTextField.text] isEqualToString:@""]) {
+        [self showAlert:@"未打卡日期不能为空"];
+        
+        return NO;
+    }
+    
+    if([[Util trimString:self.notPunchTextView.text] isEqualToString:@""] ||
+       [[Util trimString:self.explainTextView.text] isEqualToString:@""]) {
+        [self showAlert:@"内容不能为空"];
+        
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (void)report
+{
+    if([self checkValidity]) {
+        [self.view endEditing:YES];
+        
+    }
+}
+
+#pragma mark - UIScrollViewDelegate Methods
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self.view endEditing:YES];
+}
+
+#pragma mark - UITextFieldDelegate Methods
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if(textField == self.numberTextField) {
+        [self.numberTextField resignFirstResponder];
+        [self.dateTextField becomeFirstResponder];
+    } else if(textField == self.dateTextField) {
+        [self.dateTextField resignFirstResponder];
+    }
+    
+    return YES;
+}
+
+#pragma mark - UITextViewDelegate Methods
+- (BOOL)textViewShouldBeginEditing:(UITextView *)textView
+{
+    return YES;
+}
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    if(textView == self.notPunchTextView) {
+        if([Util isEmptyString:self.notPunchTextView.text]) {
+            self.notPunchPlaceholderLabel.alpha = 1.0;
+        } else {
+            self.notPunchPlaceholderLabel.alpha = 0.0;
+        }
+    } else if(textView == self.explainTextView) {
+        if([Util isEmptyString:self.explainTextView.text]) {
+            self.explainPlaceholderLabel.alpha = 1.0;
+        } else {
+            self.explainPlaceholderLabel.alpha = 0.0;
+        }
+    }
 }
 
 @end
