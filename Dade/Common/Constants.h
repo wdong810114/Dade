af5cd9f81747cec1649b29f59d7b119e39b9c7e2
王冬冬 <wdong810114@sina.com>
2015-02-02 19:48:46 +0800

    Modified something.

--- Dade/Common/Constants.h
+++ Dade/Common/Constants.h
@@ -34,7 +34,8 @@
 #define ALERT_BUTTON_TITLE_CANCEL       @"取消"
 #define BAR_BUTTON_TITLE_DONE           @"完成"
 
-#define BASE_REQUEST_URL                @"http://218.60.29.216:8080/kinghorse"
-#define LOGIN_USER_REQUEST_URL          @"%@/LoginUser"         // 用户登录身份验证接口
-#define SEND_TEXT_REQUEST_URL           @"%@/sendText"          // 发送短信验证码接口
-#define QUERY_NOTICE_LIST_REQUEST_URL   @"%@/QueryNoticeList"   // 通知文件查询列表接口
+#define BASE_REQUEST_URL                        @"http://218.60.29.216:8080/kinghorse"
+#define LOGIN_USER_REQUEST_URL                  @"%@/LoginUser"                 // 用户登录身份验证接口
+#define SEND_TEXT_REQUEST_URL                   @"%@/sendText"                  // 发送短信验证码接口
+#define QUERY_NOTICE_LIST_REQUEST_URL           @"%@/QueryNoticeList"           // 通知文件查询列表接口
+#define SHOW_NOTICE_VIEW_BY_ID_REQUEST_URL      @"%@/ShowNoticeViewById"        // 通知详细信息接口
--- Dade/Main/Notice/NoticeDetailViewController.m
+++ Dade/Main/Notice/NoticeDetailViewController.m
@@ -11,43 +11,22 @@
 @interface NoticeDetailViewController ()
 
 - (void)initView;
-- (void)refreshView;
+
+- (void)showNoticeViewById;
+- (void)requestShowNoticeViewByIdFinished:(ASIHTTPRequest *)request;
+- (void)requestShowNoticeViewByIdFailed:(ASIHTTPRequest *)request;
 
 @end
 
 @implementation NoticeDetailViewController
-{
-    NSString *_subject;     // 主题
-    NSString *_department;  // 部门
-    NSString *_quarters;    // 岗位
-    NSString *_undertaker;  // 承办
-    NSString *_recipients;  // 接收人员
-    NSString *_content;     // 内容
-}
-
-// 测试---Start
-- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if(self) {
-        _subject = @"关于第三季度综合管理检查的通知";
-        _department = @"房地产综合管理部";
-        _quarters = @"房地产部门主管";
-        _undertaker = @"卢帅";
-        _recipients = @"葛立群;崔明东;葛立群;崔明东;葛立群;崔明东;葛立群;崔明东;葛立群;崔明东;葛立群;崔明东;";
-        _content = @"各位领导、同事：\n工作汇报可以按照日程表的格式来提交，在领度企业执行与沟通平台的日程表可以用来填报自己的工作，可以按天，按小时填写汇报工作，填写的内容包含工作摘要、内容、花费时间、对应的任务、上传附件等。";
-    }
-    
-    return self;
-
-}
-// 测试---End
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
     [self initView];
+    
+    [self showNoticeViewById];
 }
 
 - (void)didReceiveMemoryWarning
@@ -81,18 +60,45 @@
         self.recipientsLabel.preferredMaxLayoutWidth = self.recipientsLabel.bounds.size.width;
         self.contentLabel.preferredMaxLayoutWidth = self.contentLabel.bounds.size.width;
     }
-    
-    [self refreshView];
 }
 
-- (void)refreshView
+- (void)showNoticeViewById
 {
-    self.subjectLabel.text = _subject;
-    self.departmentLabel.text = _department;
-    self.quartersLabel.text = _quarters;
-    self.undertakerLabel.text = _undertaker;
-    self.recipientsLabel.text = _recipients;
-    self.contentLabel.text = _content;
+    [self addLoadingView];
+    
+    NSString *postString = [NSString stringWithFormat:@"{id:'%@'}", self.noticeId];
+    NSMutableData *postData = [[NSMutableData alloc] initWithData:[postString dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    ASIFormDataRequest *request = [self requestWithRelativeURL:SHOW_NOTICE_VIEW_BY_ID_REQUEST_URL];
+    [request setPostBody:postData];
+    [self startRequest:request didFinishSelector:@selector(requestShowNoticeViewByIdFinished:) didFailSelector:@selector(requestShowNoticeViewByIdFailed:)];
+}
+
+- (void)requestShowNoticeViewByIdFinished:(ASIHTTPRequest *)request
+{
+    [self removeLoadingView];
+    
+    NSString *jsonString = request.responseString;
+    
+    NSError *error = nil;
+    NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:&error];
+    if(!error && jsonDict) {
+        self.subjectLabel.text = [jsonDict stringForKey:@"displayvalue"];
+        self.departmentLabel.text = [jsonDict stringForKey:@"depname"];
+        self.quartersLabel.text = [jsonDict stringForKey:@"orgName"];
+        self.undertakerLabel.text = [jsonDict stringForKey:@"undertake"];
+        self.recipientsLabel.text = @"";
+        self.contentLabel.text = [jsonDict stringForKey:@"content"];
+    }
+    
+    [self requestDidFinish:request];
+}
+
+- (void)requestShowNoticeViewByIdFailed:(ASIHTTPRequest *)request
+{
+    [self removeLoadingView];
+    
+    [self requestDidFail:request];
 }
 
 @end
