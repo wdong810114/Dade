eddecbec2f27bd4b6dd80768a157add05fe7f16d
王冬冬 <wdong810114@sina.com>
2015-02-05 15:51:56 +0800

    Modified todo detail page.

--- Dade/Main/Todo/TodoDetailViewController.h
+++ Dade/Main/Todo/TodoDetailViewController.h
@@ -5,13 +5,30 @@
 //  Created by 王冬冬 on 15-2-5.
 //  Copyright (c) 2015年 Spark. All rights reserved.
 //
-//  待办内容页
+//  待办内容页（正常）
 
 #import "BaseViewController.h"
 
 @interface TodoDetailViewController : BaseViewController
 
 @property (weak, nonatomic) IBOutlet UIScrollView *todoDetailScrollView;
+
+@property (weak, nonatomic) IBOutlet UILabel *subjectLabel;
+@property (weak, nonatomic) IBOutlet UILabel *typeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *departmentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *quartersLabel;
+@property (weak, nonatomic) IBOutlet UILabel *undertakerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *contentLabel;
+@property (weak, nonatomic) IBOutlet UIView *explainView;
+@property (weak, nonatomic) IBOutlet UITextView *explainTextView;
+@property (weak, nonatomic) IBOutlet UILabel *explainPlaceholderLabel;
+@property (weak, nonatomic) IBOutlet UIButton *verifyButton;
+@property (weak, nonatomic) IBOutlet UIButton *retreatButton;
+@property (weak, nonatomic) IBOutlet UIView *approvalView;
+
+- (IBAction)verifyButtonClicked:(UIButton *)button;
+- (IBAction)retreatButtonClicked:(UIButton *)button;
 
 @property (copy, nonatomic) NSString *todoId;
 
